6d9073a36612a6b7e23c519a2a710c91d20ea166
Vansh Dhar <vanshdhar@iisc.ac.in>
2024-01-16 11:29:19 +0530

    edit

--- opencv/main.py
+++ opencv/main.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.metrics import Precision, Recall, CategoricalAccuracy
+from tensorflow.keras.models import load_model
 
 
 
@@ -66,10 +67,14 @@ def scale_data(data):
     scaled_data = data.map(lambda x, y: (x / 255, y))
     batch = scaled_data.as_numpy_iterator().next()
     return f'After resizing -> max = {batch[0].max()}, min = {batch[0].min()}'
+scale_data(data)
 
-train_size = int(len(data)*.7)+1
-val_size = int(len(data)*.2)
-test_size = int(len(data)*.1)+1
+
+train_size = int(len(data)*.9)+1
+val_size = int(len(data)*.1) - 1
+test_size = int(len(data)*.1) - 3 
+print(len(data))
+print(train_size, val_size, test_size)
 
 def train_val_test_data(data, train_size: int, val_size:int, test_size: int):
     return data.take(train_size), data.skip(train_size).take(val_size), data.skip(train_size+val_size).take(test_size)
@@ -88,7 +93,11 @@ model.add(MaxPooling2D())
 model.add(Conv2D(32, (3,3), 1, activation='relu'))
 model.add(MaxPooling2D())
 
-#layer 3
+# layer 3
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+#layer 4
 model.add(Conv2D(16, (3,3), 1, activation='relu'))
 model.add(MaxPooling2D())
 
@@ -107,31 +116,31 @@ model.compile('adam', loss=tf.losses.CategoricalCrossentropy(), metrics=['accura
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-#hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
 
-# Evaluate
+#Evaluate
 
-# pre = Precision()
-# recall = Recall()
-# cat_acc = CategoricalAccuracy()
+pre = Precision()
+recall = Recall()
+cat_acc = CategoricalAccuracy()
 
-# for batch in test.as_numpy_iterator():
-#     X, y = batch
-#     yhat = model.predict(X)
-#     pre.update_state(y, yhat)
-#     recall.update_state(y ,yhat)
-#     cat_acc.update_state(y, yhat)
-# print(f'Precision{pre.result()}, Recall {recall.result()}, Accuracy {cat_acc.result()}')
+for batch in test.as_numpy_iterator():
+    X, y = batch
+    yhat = model.predict(X)
+    pre.update_state(y, yhat)
+    recall.update_state(y ,yhat)
+    cat_acc.update_state(y, yhat)
+print(f'Precision{pre.result()}, Recall {recall.result()}, Accuracy {cat_acc.result()}')
 
 
 
-# Testing data
+# # Testing data
 
-img = cv.imread('coalimage.jpeg')
+img = cv.imread('sandstone.jpeg')
 plt.imshow(img)
 
 resize = tf.image.resize(img, (256, 256))
-plt.imshow(resize.numpy().astype(int))
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
 plt.show()
 
 #Prediction
@@ -140,5 +149,16 @@ yhat = model.predict(np.expand_dims(resize / 255, 0))
 one_hot_predictions = np.eye(yhat.shape[1])[np.argmax(yhat, axis=1)]
 print(one_hot_predictions)
 
+# # Save the model
 
+# model.save(os.path.join('models', 'rockclassifier.h5'))
 
+# # Load the new model
+
+# new_model = load_model('./models/rockclassifier.h5')
+
+# yhatnew = new_model.predict(np.expand_dims(resize/255, 0))
+# one_hot_predictions = np.eye(yhatnew.shape[1])[np.argmax(yhatnew, axis=1)]
+# print(one_hot_predictions)
+
+#Class Names: ['Basalt', 'Coal', 'Granite', 'Limestone', 'Marble', 'Quartzite', 'Sandstone']
